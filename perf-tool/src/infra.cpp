dd70f4fe2664427f4dd0ca5cb41576e5afa9cfc8
Harrison Peters <hmp@ualberta.ca>
2020-12-04 02:24:32 -0700

    jlm in progress

--- perf-tool/src/infra.cpp
+++ perf-tool/src/infra.cpp
@@ -9,6 +9,10 @@
 
 Server *server;
 
+void throwError(const char* message){
+    fprintf(stderr, "%s\n", message);
+    throw("OOPS");
+}
 
 
 
@@ -22,6 +26,104 @@ void check_jvmti_error(jvmtiEnv *jvmti, jvmtiError errnum, const char *str)
         printf("ERROR: JVMTI: [%d] %s - %s", errnum, (errnum_str == NULL ? "Unknown" : errnum_str), (str == NULL ? "" : str));
         throw "Oops!";
     }
+}
+
+void startJlmEvents(jvmtiEnv *jvmti)
+{
+    jvmtiError err = JVMTI_ERROR_NONE;
+    jint extensionFunctionCount = 0;
+    jvmtiExtensionFunctionInfo *extensionFunctions = NULL;
+    int i = 0;
+
+    err == jvmti->GetExtensionFunctions(&extensionFunctionCount, &extensionFunctions);
+    if (err != JVMTI_ERROR_NONE)
+    {
+        throwError("Could not retrieve extension functions");
+    }
+    else
+    {
+
+        while (i++ < extensionFunctionCount)
+        {
+            jvmtiExtensionFunction function = extensionFunctions->func;
+
+            if (strcmp(extensionFunctions->id, COM_IBM_SET_VM_JLM) == 0)
+            {
+                /* found the set vm jlm function, so set it */
+                err = function(jvmti, COM_IBM_JLM_START);
+                if (err != JVMTI_ERROR_NONE)
+                {
+                    throwError("Could not enable JLM");
+                }
+            }
+            extensionFunctions++;
+        }
+    }
+    return;
+}
+
+void endJlmEvents(jvmtiEnv *jvmti)
+{
+    jvmtiError err = JVMTI_ERROR_NONE;
+    jint extensionFunctionCount = 0;
+    jvmtiExtensionFunctionInfo *extensionFunctions = NULL;
+    int i = 0;
+    int dumpOffset = 0; // because dump version is printed
+    jvmti->GetExtensionFunctions(&extensionFunctionCount, &extensionFunctions);
+    if (err != JVMTI_ERROR_NONE)
+    {
+        throwError("Could not retrieve extension functions");
+    }
+    else
+    {
+        while (i++ < extensionFunctionCount)
+        {
+            jvmtiExtensionFunction function = extensionFunctions->func;
+
+            if (strcmp(extensionFunctions->id, COM_IBM_JLM_DUMP_STATS) == 0)
+            {
+                /* found the setdump stats function */
+                jlm_dump *ptr = NULL;
+                char* pos = NULL;
+
+                // call for a dump
+                err = function(jvmti, &ptr, COM_IBM_JLM_DUMP_FORMAT_OBJECT_ID);
+                if (err != JVMTI_ERROR_NONE)
+                    throwError("Could not call a jlm dump");
+
+                printf("BEGIN - END: %ld\n", (ptr->end - ptr->begin) - dumpOffset);
+                for (int i = 0; i < ptr->end - ptr->begin; i++)
+                    printf("%c",((jlm_dump *)ptr)->begin[i]);
+                printf("\n");
+
+                pos = (char*) ptr->begin + dumpOffset;
+
+
+                if(*((uint8_t*)pos++) == 0x01){
+                    printf("JAVA MONITOR\n");
+                } else{
+                    printf("RAW MONITOR\n");
+                }
+
+                uint64_t location = 0;
+                location += (*pos++ & 0xFFFFFFFFFFFFFFFFu) << 0;
+                location += (*pos++ & 0xFFFFFFFFFFFFFFFFu) << 8;
+                location += (*pos++ & 0xFFFFFFFFFFFFFFFFu) << 16;
+                location += (*pos++ & 0xFFFFFFFFFFFFFFFFu) << 24;
+                location += (*pos++ & 0xFFFFFFFFFFFFFFFFu) << 32;
+                location += (*pos++ & 0xFFFFFFFFFFFFFFFFu) << 40;
+                location += (*pos++ & 0xFFFFFFFFFFFFFFFFu) << 48;
+                location += (*pos   & 0xFFFFFFFFFFFFFFFFu) << 56;
+                char* name = (char *) location;
+                printf("name: %ld\n", location);
+
+                
+                
+            }
+            extensionFunctions++;
+        }
+    }
+    return;
 }
 
 jthread createNewThread(JNIEnv *jni_env)
@@ -55,12 +157,14 @@ JNIEXPORT void JNICALL VMInit(jvmtiEnv *jvmtiEnv, JNIEnv *jni_env, jthread threa
     server = new Server(portNo, commandsPath, logPath);
 
     error = jvmtiEnv->RunAgentThread(createNewThread(jni_env), &startServer, portPointer, JVMTI_THREAD_NORM_PRIORITY);
+    startJlmEvents(jvmtiEnv);
     check_jvmti_error(jvmtiEnv, error, "Error starting agent thread\n");
     printf("VM starting up\n");
 }
 
 JNIEXPORT void JNICALL VMDeath(jvmtiEnv *jvmtiEnv, JNIEnv *jni_env)
 {
+    endJlmEvents(jvmtiEnv);
     server->shutDownServer();
     printf("VM shutting down\n");
 }
--- perf-tool/src/monitor.cpp
+++ perf-tool/src/monitor.cpp
@@ -8,6 +8,55 @@
 #include "json.hpp"
 //how many times
 using json = nlohmann::json;
+
+
+/* ENDIAN_HELPERS */
+
+#ifndef SWAP_2BYTES
+#define SWAP_2BYTES(v)  (\
+						(((v) >> (8*1)) & 0x00FF) | \
+						(((v) << (8*1)) & 0xFF00)   \
+						)
+#endif
+
+#ifndef SWAP_4BYTES
+#define SWAP_4BYTES(v) (\
+					   (((v) >> (8*3)) & 0x000000FF) | \
+					   (((v) >> (8*1)) & 0x0000FF00) | \
+					   (((v) << (8*1)) & 0x00FF0000) | \
+					   (((v) << (8*3)) & 0xFF000000)   \
+					   )
+#endif
+
+#ifndef SWAP_8BYTES
+#define SWAP_8BYTES(v) (\
+						(((v) >> (8*7)) & 0x000000FF) | \
+						(((v) >> (8*5)) & 0x0000FF00) | \
+						(((v) >> (8*3)) & 0x00FF0000) | \
+						(((v) >> (8*1)) & 0xFF000000) | \
+						(((v) & 0xFF000000) << (8*1)) | \
+						(((v) & 0x00FF0000) << (8*3)) | \
+						(((v) & 0x0000FF00) << (8*5)) | \
+						(((v) & 0x000000FF) << (8*7)) \
+					   )
+#endif
+
+#ifdef J9VM_ENV_LITTLE_ENDIAN
+#define FORCE_LITTLE_ENDIAN_2BYTES(val) SWAP_2BYTES((val))
+#define FORCE_LITTLE_ENDIAN_4BYTES(val) SWAP_4BYTES((val))
+#define FORCE_LITTLE_ENDIAN_8BYTES(val) SWAP_8BYTES((U_64)(val))
+#else
+#define FORCE_LITTLE_ENDIAN_2BYTES(val) (val)
+#define FORCE_LITTLE_ENDIAN_4BYTES(val) (val)
+#define FORCE_LITTLE_ENDIAN_8BYTES(val) (val)
+#endif
+
+#define READ_1BYTE(val)   (val) =  *((U_8*) val;                      dump++;
+#define READ_2BYTES(val) *((U_16*)dump) = FORCE_BIG_ENDIAN_2BYTES(val); dump += sizeof(U_16);
+#define READ_4BYTES(val) *((U_32*)dump) = FORCE_BIG_ENDIAN_4BYTES((U_32)val); dump += sizeof(U_32);
+#define READ_8BYTES(val) *((U_64*)dump) = FORCE_BIG_ENDIAN_8BYTES(val); dump += sizeof(U_64);
+
+
 struct ClassCycleInfo
 {
     int numFirstTier;
@@ -18,58 +67,105 @@ std::atomic<bool> stackTraceEnabled{true};
 std::atomic<int> monitorSampleRate{1};
 std::atomic<int> monitorSampleCount{0};
 
+void throwError(const char* message){
+    fprintf(stderr, "%s\n", message);
+    throw("OOPS");
+}
+
 void startJlmEvents(jvmtiEnv *jvmti)
 {
-    jvmtiError err;
+    jvmtiError err = JVMTI_ERROR_NONE;
     jint extensionFunctionCount = 0;
     jvmtiExtensionFunctionInfo *extensionFunctions = NULL;
     int i = 0;
 
-    jvmti->GetExtensionFunctions(&extensionFunctionCount, &extensionFunctions);
-
-
-    while (i++ < extensionFunctionCount)
+    err == jvmti->GetExtensionFunctions(&extensionFunctionCount, &extensionFunctions);
+    if (err != JVMTI_ERROR_NONE)
     {
-        jvmtiExtensionFunction function = extensionFunctions->func;
-
-        if (strcmp(extensionFunctions->id, COM_IBM_SET_VM_JLM) == 0)
-        {
-            /* found the set vm jlm function */
-            err = function(jvmti, COM_IBM_JLM_START);
-        }
-        extensionFunctions++;
+        throwError("Could not retrieve extension functions");
     }
+    else
+    {
 
+        while (i++ < extensionFunctionCount)
+        {
+            jvmtiExtensionFunction function = extensionFunctions->func;
+
+            if (strcmp(extensionFunctions->id, COM_IBM_SET_VM_JLM) == 0)
+            {
+                /* found the set vm jlm function, so set it */
+                err = function(jvmti, COM_IBM_JLM_START);
+                if (err != JVMTI_ERROR_NONE)
+                {
+                    throwError("Could not enable JLM");
+                }
+            }
+            extensionFunctions++;
+        }
+    }
     return;
 }
 
 void endJlmEvents(jvmtiEnv *jvmti)
 {
-    jvmtiError err;
+    jvmtiError err = JVMTI_ERROR_NONE;
     jint extensionFunctionCount = 0;
     jvmtiExtensionFunctionInfo *extensionFunctions = NULL;
     int i = 0;
+    int dumpOffset = 8; // because dump version is printed
     jvmti->GetExtensionFunctions(&extensionFunctionCount, &extensionFunctions);
-
-    while (i++ < extensionFunctionCount)
+    if (err != JVMTI_ERROR_NONE)
     {
-        jvmtiExtensionFunction function = extensionFunctions->func;
-
-        if (strcmp(extensionFunctions->id, COM_IBM_SET_VM_JLM_DUMP) == 0)
+        throwError("Could not retrieve extension functions");
+    }
+    else
+    {
+        while (i++ < extensionFunctionCount)
         {
-            /* found the set vm jlm function */
-            void *ptr;
-            err = function(jvmti, &ptr);
-            if(err == 1)
-                throw("Ooops!");
+            jvmtiExtensionFunction function = extensionFunctions->func;
 
-            
-            printf("BEGIN - END: %ld\n", ((jlm_dump *)ptr)->end - ((jlm_dump *)ptr)->begin );
-            for(int i = 0; i < ((jlm_dump *)ptr)->end - ((jlm_dump *)ptr)->begin; i++)
-                printf("%x", ((jlm_dump *)ptr)->begin[i]);
-            printf("\n");
+            if (strcmp(extensionFunctions->id, COM_IBM_JLM_DUMP_STATS) == 0)
+            {
+                /* found the setdump stats function */
+                jlm_dump *ptr = NULL;
+                char* pos = NULL;
+
+                // call for a dump
+                err = function(jvmti, &ptr, COM_IBM_JLM_DUMP_FORMAT_OBJECT_ID);
+                if (err != JVMTI_ERROR_NONE)
+                    throwError("Could not call a jlm dump");
+
+                printf("BEGIN - END: %ld\n", (ptr->end - ptr->begin) - dumpOffset);
+                for (int i = 0; i < ptr->end - ptr->begin; i++)
+                    printf("%x", ((jlm_dump *)ptr)->begin[i]);
+                printf("\n");
+
+                pos = (char*) ptr->begin + dumpOffset;
+
+
+                if(*((uint8_t*)pos++) == 0x01){
+                    printf("JAVA MONITOR\n");
+                } else{
+                    printf("RAW MONITOR\n");
+                }
+
+                uint64_t location = 0;
+                location += (*pos++ & 0xFFFFFFFFFFFFFFFFu) << 0;
+                location += (*pos++ & 0xFFFFFFFFFFFFFFFFu) << 8;
+                location += (*pos++ & 0xFFFFFFFFFFFFFFFFu) << 16;
+                location += (*pos++ & 0xFFFFFFFFFFFFFFFFu) << 24;
+                location += (*pos++ & 0xFFFFFFFFFFFFFFFFu) << 32;
+                location += (*pos++ & 0xFFFFFFFFFFFFFFFFu) << 40;
+                location += (*pos++ & 0xFFFFFFFFFFFFFFFFu) << 48;
+                location += (*pos   & 0xFFFFFFFFFFFFFFFFu) << 56;
+                char* name = (char *) location;
+                printf("name: %ld\n", location);
+
+                
+                
+            }
+            extensionFunctions++;
         }
-        extensionFunctions++;
     }
     return;
 }
@@ -94,7 +190,8 @@ void setMonitorSampleRate(int rate)
     }
 }
 
-JNIEXPORT void JNICALL MonitorContendedEntered(jvmtiEnv *jvmtiEnv, JNIEnv *env, jthread thread, jobject object){
+JNIEXPORT void JNICALL MonitorContendedEntered(jvmtiEnv *jvmtiEnv, JNIEnv *env, jthread thread, jobject object)
+{
     startJlmEvents(jvmtiEnv);
     json j;
     jvmtiError error;
@@ -116,7 +213,6 @@ JNIEXPORT void JNICALL MonitorContendedEntered(jvmtiEnv *jvmtiEnv, JNIEnv *env, 
     if (str != NULL && error == JVMTI_ERROR_NONE) */
     // record calling class
     j["Class"] = str;
-
 
     numContentions[str].numFirstTier++;
 
@@ -147,7 +243,7 @@ JNIEXPORT void JNICALL MonitorContendedEntered(jvmtiEnv *jvmtiEnv, JNIEnv *env, 
             }
         }
         monitorSampleCount++;
-    } 
+    }
     // printf("%s\n", j.dump().c_str());
     sendToServer(j.dump());
     endJlmEvents(jvmtiEnv);
